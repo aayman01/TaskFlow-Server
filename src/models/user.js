b49ddb5f21d405e1a61819c88906c78c79348619
aayman01 <ayman.alfi098@gmail.com>
2025-03-08 15:24:08 +0600

    user schema created

---
+++ src/models/user.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    isFaActive: {
+      type: Boolean,
+      required: false,
+    },
+    twoFactorSecret: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
